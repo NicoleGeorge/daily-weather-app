49b08869e8cb7f867a106ccf8709e85957468730
Nicole George <hello@nicolegeorge.me>
2020-03-31 22:49:57 +1030

    current weather working, search field validtion complete, styles added

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,6 +2,7 @@
 $(document ).ready(function() {
 
 // API call format: api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// const uvURL = "https://http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
 // API key: 4628463fa26bf6e9e88de6363b182cb3
 
 const url = "http://api.openweathermap.org/data/2.5/weather?q=";
@@ -16,6 +17,7 @@ const url = "http://api.openweathermap.org/data/2.5/weather?q=";
 
     function getWeather(){
         const city = $('#inputCity').val();
+
         // validation - city must be entered 
         if (city != ''){
 
@@ -23,50 +25,29 @@ const url = "http://api.openweathermap.org/data/2.5/weather?q=";
                 url: url + city + "&units=metric" + "&appid=4547d4d0a5accd3b6ae29ab1abbcddfc",
                 type: "GET",
                 dataType: "jsonp",
-                success: function(data) {
-                    console.log(data)
+                success: function(result) {
+                    let widget = displayData(result)
+                    $("#displayWeather").html(widget);
+                    $("#inputCity").val('');
                 } 
             })
 
 
         } else {
-            $('#errorMessage').html("<div>Please enter a city name to continue.</div>");
+            // adding bootstrap classes to the error message - alert & close X button
+            $('#errorMessage').html("<div class='alert alert-danger' id='inputError'>Please enter a city name to continue.</div>");
         }
     }
 
-// $.ajax ({
-//     url: url + city + "&units=metric" + "&appid=4547d4d0a5accd3b6ae29ab1abbcddfc",
-//     success: function(result){
-//         console.log(result);
-//         console.log(result.name);
-
-//     // Stage 1: current location output
-//     let displayCity = `Current weather in: ${result.name}`;
-//         $('#location').html(displayCity);
-
-//     // Stage 3: Temperature - converting kelvin (default) to celsius
-//     // let C = result.main.temp - 273.15;
-
-//     // use Math.round method to convert output into a round number 
-//         let C = Math.round(result.main.temp - 273.15);
-//         // optimising the code + used https://altcodes.net to find alt codes
-//         let displayTemp = `Temperature: ${C}&#176;C`
-//         $('#temp').html(displayTemp);
-    
-//     // Stage 5: humidity
-//         let displayHumidity = `Humidity: ${result.main.humidity}%`
-//         $('#humidity').html(displayHumidity);
-
-//     // Stage 4: wind speed output (default = mps) - don't need to convert it.
-//         let displayWind = `Wind: ${result.wind.speed} mps`;
-//         $('#wind').html(displayWind);
-
-//     // Stage 2: 
-//     // sky conditions is the 3rd index of the weather object array
-//         let displaySky = `Sky Conditions: ${result.weather[0].description}`;
-//         $('#sky').html(displaySky);
-//     }
-
-// })
+    function displayData(result) { 
+        // returning data values + country name & country code output
+        return  '<h3>Current Weather for: ' + result.name + ', ' + result.sys.country+' </h3>' +
+                "<h4>Temperature: " + result.main.temp + "&deg;C </h4>" +
+                "<h4>Humidity: " + result.main.humidity + "% </h4>" +
+                "<h4>Wind: " + result.wind.speed + " m/s </h4>" + 
+                "<h4>Sky Conditions: " + result.weather[0].description + "</h4>" +
+                "<h4>UV Index: " + "</h4>";
+                
+    }
 
 });
