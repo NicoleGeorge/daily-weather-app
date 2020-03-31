3ed98227044ce522adc9df5a76a53174f55c982f
Nicole George <hello@nicolegeorge.me>
2020-03-31 16:51:55 +1030

    current weather data working and outputing onto page. first stage complete

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -10,7 +10,35 @@ $.ajax ({
     url: url,
     success: function(result){
         console.log(result);
+        console.log(result.name);
+
+    // Stage 1: 
+    // current location output
+        $('#location').text(result.name);
+
+    // Stage 3: Temperature - converting kelvin (default) to celsius
+    // let C = result.main.temp - 273.15;
+
+    // use Math.round method to convert output into a round number 
+        let C = Math.round(result.main.temp - 273.15);
+        // converting the output back into readable number from binary
+        let celsius = C.toString();
+        $('#temp').text(celsius);
+    
+    // Stage 5: humidity
+
+        $('#humidity').text(result.main.humidity);
+
+    // Stage 4: wind speed output (default = mps) - don't need to convert it.
+
+        $('#wind').text(result.wind.speed);
+
+    // Stage 2: 
+    // sky conditions is the 3rd index of the weather object array
+
+        $('#sky').text(result.weather[0].description);
     }
-})
 
 })
+
+});
